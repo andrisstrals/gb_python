efb8e6e6043d9167c713abfcda6afa3743abffcf
Andris Strals <andris.strals@gmail.com>
2021-01-13 21:45:02 +0000

    task 2

--- homework_05/task_02.py
+++ homework_05/task_02.py
@@ -1,2 +1,11 @@
 # Создать текстовый файл (не программно), сохранить в нём несколько строк,
 # выполнить подсчёт строк и слов в каждой строке.
+
+rows = 0
+with open('task2.txt') as file:
+    for line in file:
+        rows += 1
+        print(f'Row:{rows:2d} - words:{len(line.split()):2d}')
+
+
+print(f'Total rows: {rows}')
