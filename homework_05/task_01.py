de0bd5f699191c266de9d2bd8a3bcbca81c68e6d
Andris Strals <andris.strals@gmail.com>
2021-01-13 21:26:16 +0000

    task 01

--- homework_05/task_01.py
+++ homework_05/task_01.py
@@ -1,2 +1,9 @@
 # Создать программный файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
 # Об окончании ввода данных будет свидетельствовать пустая строка.
+
+with open('task01.tmp', 'w') as file:
+    while True:
+        s = input('Enter something or empty line to finish:')
+        if not s:
+            break
+        print(s, file=file)
