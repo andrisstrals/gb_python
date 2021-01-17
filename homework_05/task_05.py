1858de1e31efc36cfce7cf92086f1c8c3a7b2d96
Andris Strals <andris.strals@gmail.com>
2021-01-17 20:31:11 +0000

    task 5

--- homework_05/task_05.py
+++ homework_05/task_05.py
@@ -1,2 +1,20 @@
 # Создать (программно) текстовый файл, записать в него программно набор чисел, разделённых пробелами.
 # Программа должна подсчитывать сумму чисел в файле и выводить её на экран.
+
+import random as rnd
+from functools import reduce
+
+filename = 'task5.tmp'
+count = 20
+
+with open(filename, 'w') as f:
+    for i in range(count):
+        f.write(str(rnd.randint(-100, 100)) + ' ')
+
+with open(filename) as f:
+    s = f.read()
+
+lst = list(map(int, s.split()))
+
+print(reduce(lambda a, b: a + b, lst))
+
