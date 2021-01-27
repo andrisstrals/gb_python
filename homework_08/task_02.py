7498c54fb2662fc809843171e2f650a932bc4079
Andris Strals <andris.strals@gmail.com>
2021-01-27 23:32:38 +0000

    task 2

--- homework_08/task_02.py
+++ homework_08/task_02.py
@@ -2,3 +2,32 @@
 # Проверьте его работу на данных, вводимых пользователем.
 # При вводе пользователем нуля в качестве делителя программа должна корректно обработать
 # эту ситуацию и не завершиться с ошибкой.
+
+class ErrorDivByZero(Exception):
+    def __init__(self, num):
+        self.num = num
+
+    def __str__(self):
+        return f"Don't even think of dividing {self.num} by ZERO!"
+
+def divide(a, b):
+    if not b:
+        raise ErrorDivByZero(a)
+
+    return a / b
+
+
+
+while True:
+    s = input('Enter two numbers or emty line to finish: ')
+    if not s:
+        break
+
+    try:
+        nums = list(map(int, s.split()))
+        print(f'{nums[0]} / {nums[1]} = {divide(nums[0], nums[1])}')
+    except ErrorDivByZero as e:
+        print(f'ERROR: {e}')
+    except Exception as e:
+        print(f'Something went wrong: {e}')
+
